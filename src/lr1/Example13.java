e76938338ed5807dfb9cecd1d91a7359d34ebf0e
KrasinAnton <2saper@mail.ru>
2022-11-27 00:31:38 +0500

    initial commit

--- src/lr1/Example13.java
+++ src/lr1/Example13.java
@@ -1,6 +1,22 @@
 package lr1;
 
+import java.util.Scanner;
+
 public class Example13 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int firstNumber; int secondNumber ;
+        int sum;
+        System.out.println("Введите первое число: ");
+        firstNumber = in.nextInt();
+        System.out.println("Введите второе число: ");
+        secondNumber = in.nextInt();
+
+        sum = firstNumber + secondNumber;
+
+        System.out.println("Сумма двух чисел равна: " +sum);
+        in.close();
+    }
 
 }
 /*Напишите программу для вычисления суммы двух чисел.
--- src/lr1/Example15.java
+++ src/lr1/Example15.java
@@ -1,6 +1,28 @@
 package lr1;
 
+import java.util.Scanner;
+
 public class Example15 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int firstNumber; int secondNumber;
+        int sum; int sum2;
+
+        System.out.println("Введите первое число: ");
+        firstNumber = in.nextInt();
+        System.out.println("Введите второе число: ");
+        secondNumber = in.nextInt();
+
+        sum = firstNumber + secondNumber;
+        sum2 = firstNumber - secondNumber;
+
+        System.out.println("Сумма этих чисел: "  +sum);
+        System.out.println("Разность этих чисел: " +sum2);
+        in.close();
+    }
+
 }
 /*Напишите программу, в которой Пользователь вводит два числа,
 а программой вычисляется и отображается сумма и разность этих чисел.*/
