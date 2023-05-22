fd635eb45679117ec6ee7422f0f50f80703e88f0
KrasinAnton <2saper@mail.ru>
2023-05-22 11:28:03 +0500

    lr13

---
+++ src/lr13/Task4.java
@@ -0,0 +1,18 @@
+package lr13;
+
+public class Task4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            Thread thread = new Thread(new NumberPrintingThread(i));
+            thread.start();
+        }
+    }
+
+    private record NumberPrintingThread(int threadNumber) implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Поток " + threadNumber + " запущен");
+        }
+    }
+}
